779798796b05ede35041e14b51c94ea643330609
Silyow <515185930.@qq.com>
2020-06-15 14:33:59 +0800

    添加状态提示组件

--- src/App.js
+++ src/App.js
@@ -90,6 +90,12 @@ function App() {
               <NoUpdata />
             </div>
           </div>
+          <div className="block">
+            <p className="blockTitle">状态提升(例:输入水的温度控制沸腾状态)</p>
+            <div className="blockComponent">
+              <Calculator />
+            </div>
+          </div>
         </div>
       </header>
     </div>
@@ -423,6 +429,96 @@ function NoUpdata() {
       <input type="text" value="不能更改" />
     </div>
   );
+}
+//状态提升，(即多个组件反映同一个状态变化，将state提示为父级的state控制)
+//例子水的沸腾点
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit",
+};
+
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+function Boiling(props) {
+  if (props.celsius >= 100) {
+    return <p>水沸腾了!</p>;
+  } else {
+    return <p>水还未沸腾!</p>;
+  }
+}
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeValue = this.changeValue.bind(this);
+  }
+  changeValue(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>改变水的{scaleNames[scale]}度数:</legend>
+        <input value={temperature} onChange={this.changeValue} />
+      </fieldset>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { temperature: "", scale: "c" };
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+  }
+  handleCelsiusChange(temperature) {
+    this.setState({ scale: "c", temperature });
+  }
+  handleFahrenheitChange(temperature) {
+    this.setState({ scale: "f", temperature });
+  }
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius =
+      scale == "c" ? temperature : tryConvert(temperature, toFahrenheit);
+    const fahrenheit =
+      scale == "f" ? temperature : tryConvert(temperature, toCelsius);
+    return (
+      <div>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <Boiling celsius={parseFloat(celsius)} />
+      </div>
+    );
+  }
 }
 //react变量提升
 export default App;
